c389db9bc82b82cc93b41de6a7b304da0930d759
duongpixel <truongminhduong114@gmail.com>
2022-10-01 19:59:02 +0700

    ss5

---
+++ 3-Classwork/Js/ss5.js
@@ -0,0 +1,38 @@
+let a = 12.5;
+let b = 'rn26-duongtm';
+let c = true;
+console.log('Score ',a);
+console.log('Class Name ',b);
+console.log('23 Years old ',c);
+function eventClick(){
+    alert('Click F12 and Choose Console');
+}
+let obj = {
+    Name : 'Truong Minh Duong',
+    Age : 23,
+    Work : 'MMAX',
+    Business:'VACS'
+}
+console.log(obj.Business);
+const arr = [
+    {
+        name : 'Truong Minh Duong',
+        score : 3.75,
+        age: 23,
+        class:'ReactJS-26'       
+    },
+    {
+        name : 'Truong Minh Duy',
+        score: 5.7,
+        age:19,
+        class:'ReactJS-26'
+    }
+]
+console.log(arr[0]);
+console.log(arr[1]);
+
+function Change() {
+    a = a + 1;
+    console.log(a);
+    
+}
