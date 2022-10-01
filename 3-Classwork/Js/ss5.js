dc29c818f42c24b8db58238a29a31e59bebf8047
duongpixel <truongminhduong114@gmail.com>
2022-10-01 20:37:10 +0700

    ss5-update

--- 3-Classwork/Js/ss5.js
+++ 3-Classwork/Js/ss5.js
@@ -1,11 +1,11 @@
-let a = 12.5;
-let b = 'rn26-duongtm';
-let c = true;
-console.log('Score ',a);
-console.log('Class Name ',b);
-console.log('23 Years old ',c);
+let a = 12.5
+let b = 'rn26-duongtm'
+let c = true
+console.log('Score ',a)
+console.log('Class Name ',b)
+console.log('23 Years old ',c)
 function eventClick(){
-    alert('Click F12 and Choose Console');
+    alert('Click F12 and Choose Console')
 }
 let obj = {
     Name : 'Truong Minh Duong',
@@ -13,7 +13,7 @@ let obj = {
     Work : 'MMAX',
     Business:'VACS'
 }
-console.log(obj.Business);
+console.log(obj.Business)
 const arr = [
     {
         name : 'Truong Minh Duong',
@@ -24,15 +24,22 @@ const arr = [
     {
         name : 'Truong Minh Duy',
         score: 5.7,
-        age:19,
+        age: 19,
         class:'ReactJS-26'
     }
 ]
-console.log(arr[0]);
-console.log(arr[1]);
+console.log(arr[0])
+console.log(arr[1])
 
 function Change() {
-    a = a + 1;
-    console.log(a);
-    
+    a = a + 2
+    console.log(a)
 }
+
+let getClass = document.getElementsByClassName('btn_class')
+let getID = document.getElementById('btn_id')
+function changeTitle(){
+    getID.innerHTML('Minh Duong')
+}
+console.log(getClass)
+console.log(getID)
